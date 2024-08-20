405df1a4d8461d5be9799ecca1a13f7c4a8af0e4
mustafa276 <mmustafamajeed1@gmail.com>
2024-08-20 13:18:56 +0500

    type casting

---
+++ activity2.py
@@ -0,0 +1,19 @@
+name = "mustafa"
+age = 11
+weight = 42.1
+is_student = True
+print("name : ", name)
+print("data type of name is", type(name))
+print("age : ", age)
+print("data type of age is", type(age))
+print("weight : ", weight)
+print("data type of weight is", type(weight))
+print("is_student : ", is_student)
+print("data type of is_student is", type(is_student))
+print("\nAfter type casting....")
+age = str(age)
+print("age : ", age)
+print("data type of age is", type(age))
+weight = int(weight)
+print("weight : ", weight)
+print("data type of weight is", type(weight))
